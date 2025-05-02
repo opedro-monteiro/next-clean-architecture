2bbd81c39e2523abe55ce47eb64f4502c67a7c98
opedro-monteiro <pedro.oliveira@monteirodev.com>
2025-05-01 21:07:03 -0300

    feat: enhance SidebarContent with filter functionality and add formatPrice helper

--- src/components/sidebar-content.tsx
+++ src/components/sidebar-content.tsx
@@ -1,10 +1,76 @@
-import { FiltersSection } from './filters-section'
-import { SheetContent } from './ui/sheet'
+'use client'
+
+import { useFilterCategories } from '@/hooks/useFilterCategories'
+import { useFilterColors } from '@/hooks/useFilterColors'
+import { useFilterPrice } from '@/hooks/useFilterPrice'
+import { useFilterSizes } from '@/hooks/useFilterSizes'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { SectionFilterCategories } from './section-filter-categories'
+import { SectionFilterColors } from './section-filter-colors'
+import { SectionFilterPrice } from './section-filter-price'
+import { SectionFilterSizes } from './section-filter-sizes'
+import { Button } from './ui/button'
+import { Separator } from './ui/separator'
+import { SheetContent, SheetFooter, SheetHeader, SheetTitle } from './ui/sheet'
 
 export function SidebarContent() {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const { selectedSizes, toggleSize } = useFilterSizes()
+  const { selectedColors, toggleColor } = useFilterColors()
+  const { priceRange, handlePriceRangeChange } = useFilterPrice()
+  const { selectedCategories, toggleCategory } = useFilterCategories()
+
+  const handleApplyFilters = () => {
+    const params = new URLSearchParams(searchParams.toString())
+
+    params.set('min', priceRange[0].toString())
+    params.set('max', priceRange[1].toString())
+
+    router.push(`${pathname}?${params.toString()}`)
+  }
+
   return (
-    <SheetContent side={'bottom'}>
-      <FiltersSection />
+    <SheetContent side={'bottom'} className="rounded-t-lg px-5">
+      <SheetHeader className="pb-0">
+        <SheetTitle className="flex items-center justify-between text-xl font-bold">
+          Filters
+        </SheetTitle>
+      </SheetHeader>
+      {/* Filters */}
+      <Separator />
+      <SectionFilterCategories
+        selectedCategories={selectedCategories}
+        toggleCategory={toggleCategory}
+      />
+      {/* filtro imediato */}
+      <Separator />
+      <SectionFilterPrice
+        priceRange={priceRange}
+        handlePriceRangeChange={handlePriceRangeChange}
+      />
+      {/* filtro pelo apply */}
+      <Separator />
+      <SectionFilterColors
+        selectedColors={selectedColors}
+        toggleColor={toggleColor}
+      />
+      {/* filtro imediato */}
+      <Separator />
+      <SectionFilterSizes
+        selectedSizes={selectedSizes}
+        toggleSize={toggleSize}
+      />
+      <SheetFooter className="flex flex-col gap-2 pt-4">
+        <Button className="h-12 rounded-full" onClick={handleApplyFilters}>
+          Apply Filters
+        </Button>
+        <Button className="h-12 rounded-full" variant={'outline'}>
+          Reset Filters
+        </Button>
+      </SheetFooter>
     </SheetContent>
   )
 }
